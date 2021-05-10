43f1dcba7b87cd56b30f4af0bff05eac385b1799
Sebastian Andil <sebastian.andil@gmail.com>
2021-05-10 17:01:46 +0200

    Add filters

--- src/pages/Offers/Offers.jsx
+++ src/pages/Offers/Offers.jsx
@@ -2,51 +2,74 @@ import React from 'react'
 import { differenceInMonths } from 'date-fns'
 import {
   Box,
-  Icon,
-  Grid,
-  Container,
-  Text,
-  Input,
-  InputLeftElement,
-  InputGroup,
-  useToken,
-  Spinner,
   Center,
+  Checkbox,
+  Container,
+  Grid,
   Heading,
-  VStack,
-  InputRightElement,
+  Icon,
   IconButton,
-  Badge,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Radio,
+  RadioGroup,
+  SimpleGrid,
+  Spinner,
+  Stack,
+  Text,
+  useToken,
 } from '@chakra-ui/react'
 import { AiFillCloseCircle, AiOutlineSearch } from 'react-icons/ai'
+import { uniqBy, sortBy, flowRight, identity } from 'lodash-es'
 import { Offer } from '../../components/Offer'
 import { Header } from '../components/Header'
 import { useFetch } from '../../hooks'
 import bg from '../../assets/bg.jpg'
 
+const isNew = (date) => differenceInMonths(new Date(), new Date(date)) < 6
+
+const initialActiveFilters = {
+  newOnly: false,
+  country: 'all',
+}
+
+const filterByNew = (offers) => offers.filter(({ createdAt }) => isNew(createdAt))
+const filterByCountry = (country) => (offers) => offers.filter((offer) => offer.country === country)
+
 export const Offers = () => {
   const [searchTerm, setSearchTerm] = React.useState('')
-
-  const { data: rawOffers, isLoading } = useFetch('http://localhost:3004/offers?_limit=24')
-
+  const [activeFilters, setActiveFilters] = React.useState(initialActiveFilters)
+  const { data: rawOffers, isLoading } = useFetch('http://localhost:3004/offers?_limit=30')
   const offerMinWidth = useToken('sizes', Offer.minWidth)
 
   const handleSearch = React.useCallback((e) => {
     setSearchTerm(e.target.value)
   }, [])
 
-  const offers = React.useMemo(() => {
-    return searchTerm
-      ? rawOffers.filter(({ country, city }) => (country + city).toLowerCase().includes(searchTerm))
-      : rawOffers
-  }, [searchTerm, rawOffers])
+  // maybe useMemo
+  const offers = searchTerm
+    ? rawOffers.filter(({ country, city }) => (country + city).toLowerCase().includes(searchTerm))
+    : rawOffers
+
+  /**
+   * compose (Lodash's `flowRight`) example
+   * const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)))
+   */
+
+  // maybe useMemo
+  const filteredOffers = flowRight(
+    activeFilters.newOnly ? filterByNew : identity,
+    activeFilters.country !== 'all' ? filterByCountry(activeFilters.country) : identity
+  )(offers)
 
   return (
     <>
       <Header />
       <Box bg={`url('${bg}') center / cover no-repeat`}>
         <Box bg="blackAlpha.700" py="20">
-          <VStack as={Container} spacing="10">
+          <Stack as={Container} spacing="10">
             <Heading size="2xl" color="white" textAlign="center">
               Find your next adventure
             </Heading>
@@ -75,14 +98,42 @@ export const Offers = () => {
                 </InputRightElement>
               )}
             </InputGroup>
-          </VStack>
+          </Stack>
         </Box>
       </Box>
       <Container maxWidth="container.xl">
         <Grid gridTemplateColumns={['auto', '15rem auto']} gap="4">
-          <Center h="12">
-            <Badge colorScheme="red">TODO: filters</Badge>
-          </Center>
+          <Stack spacing="4" py="4">
+            <Text fontWeight="bold" textTransform="uppercase">
+              Filters
+            </Text>
+            <Checkbox
+              onChange={(e) =>
+                setActiveFilters((prevState) => ({ ...prevState, newOnly: e.target.checked }))
+              }
+            >
+              New only
+            </Checkbox>
+            <Text fontWeight="bold" textTransform="uppercase" fontSize="small">
+              Filter by country
+            </Text>
+            <RadioGroup
+              onChange={(country) => setActiveFilters((prevState) => ({ ...prevState, country }))}
+              value={activeFilters.country}
+            >
+              <SimpleGrid gap="2">
+                <Radio value="all">All</Radio>
+                {sortBy(
+                  uniqBy(offers, ({ country }) => country),
+                  ({ country }) => country
+                ).map(({ id, country }) => (
+                  <Radio key={id} value={country}>
+                    {country}
+                  </Radio>
+                ))}
+              </SimpleGrid>
+            </RadioGroup>
+          </Stack>
           {isLoading ? (
             <Center height="100vh">
               <Spinner
@@ -94,17 +145,15 @@ export const Offers = () => {
               />
             </Center>
           ) : (
-            <div>
-              <Box h="12" d="flex" alignItems="center">
-                <Text fontSize="sm" color="gray">
-                  {offers.length} offers found
-                </Text>
-              </Box>
+            <Stack spacing="4" py="4">
+              <Text fontSize="sm" color="gray">
+                {filteredOffers.length === 1 ? '1 offer' : `${filteredOffers.length} offers`} found
+              </Text>
               <Grid
                 gridTemplateColumns={`repeat(auto-fill, minmax(${offerMinWidth}, 1fr));`}
                 gap={4}
               >
-                {offers.map(
+                {filteredOffers.map(
                   ({
                     id,
                     country,
@@ -117,19 +166,20 @@ export const Offers = () => {
                     reviewCount,
                   }) => (
                     <Offer
+                      key={id}
                       destination={`${city}, ${country}`}
                       imageUrl={thumbnail}
                       formattedPrice={`$${price}`}
                       rating={rating}
                       linkTo={String(id)}
                       nights={nights}
-                      isNew={differenceInMonths(new Date(), new Date(createdAt)) < 6}
+                      isNew={isNew(createdAt)}
                       reviewCount={reviewCount}
                     />
                   )
                 )}
               </Grid>
-            </div>
+            </Stack>
           )}
         </Grid>
       </Container>
