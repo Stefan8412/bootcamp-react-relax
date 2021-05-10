408de8b2cdbf01b3240067b0b681efcc7c849691
Sebastian Andil <sebastian.andil@gmail.com>
2021-05-10 09:34:08 +0200

    Polish home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,19 +3,18 @@ import { Box, Button, Center, Heading, Stack } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 import bg from '../assets/bg.jpg'
 
-const Home = () => (
+export const Home = () => (
   <Box bg={`url('${bg}') center / cover no-repeat`} h="100vh">
     <Center h="100vh">
-      <Stack spacing="4" align="center">
-        <Heading size="3xl" color="gray.50">
-          Welcome!
+      <Stack spacing="8" align="center">
+        <Heading size="3xl" color="gray.50" textAlign="center">
+          Welcome <br />
+          to React Relax!
         </Heading>
-        <Button as={Link} to="offers">
-          See what's inside
+        <Button as={Link} to="offers" colorScheme="green" size="lg" shadow="dark-lg">
+          Ready for something new?
         </Button>
       </Stack>
     </Center>
   </Box>
 )
-
-export default Home
